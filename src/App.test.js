77617ffda96a9aa7dea84da862aa02ceec891297
tony <chcjswo@gmail.com>
2020-10-11 16:12:01 +0900

    edit test file

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Hello React link', () => {
+	const { getByText } = render(<App />);
+	const linkElement = getByText(/Hello React/i);
+	expect(linkElement).toBeInTheDocument();
 });
